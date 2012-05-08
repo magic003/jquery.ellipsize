5def7349e4b83dd20653369ec5817e08529fad2b
Minjie Zha <minjiezha@gmail.com>
2012-05-08 03:37:22 -0400

    commit source code

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 jquery.ellipsize
 ================
 
-jQuery ellipsis plugin
+This is a jQuery ellipsis plugin based on [jquery-ellipsis](https://github.com/sakura-sky/jquery-ellipsis), with the following enhancements:
+    * Using binary search to calculate the fit content
+    * Taking care of the embedded HTML markup
+
+Known Issues
+------------
+
+* It goes into an infinite loop if the cut element is a text node.
---
+++ src/jquery.ellipsize.js
@@ -0,0 +1,126 @@
+/**
+ * jQuery Ellipsis Plugin
+ *
+ * Copyright 2012 Minjie Zha
+ */
+(function($) {
+    $.fn.ellipsize = function(options) {
+        options = $.extend({
+            'ellipsis': '\u2026',
+            'multiline': false
+        }, options);
+
+        // Ellipsize the target element.
+        function fnEllipsize() {
+            var el = $(this);
+
+            if (el.css("overflow") === "hidden") {
+                var multiline = options.multiline,
+                    ellipsisText = options.ellipsis,
+                    // clone the element
+                    t = $(this.cloneNode(true))
+                        .hide()
+                        .css({
+                            'position': 'absolute',
+                            'overflow': 'visible'
+                        })
+                        .width(multiline ? el.width() : 'auto')
+                        .height(multiline ? 'auto' : el.height());
+                el.after(t);
+
+                // functions testing whether the content fits
+                function heightFit() { return el.height() >= t.height(); }
+                function widthFit() { return el.width() >= t.width(); }
+                var fit = multiline ? heightFit : widthFit;
+
+                // if already fits, do nothing
+                if(fit()) {
+                    t.remove();
+                    return;
+                }
+
+                // add ellipsis to a specific position
+                function addEllipsis(elements, length) {
+                    var els = elements.slice(0,length);
+                    var dummy = $(document.createElement('div'));
+                    for(var i=0; i < els.length; i++) {
+                        dummy.append(els.get(i));
+                    }
+                    dummy.append('<span>' + ellipsisText + '</span>');
+                    return dummy.html();
+                }
+
+                // using binary search to ellipsize the element
+                var c = t;
+                while(true) {
+                    var children = c.contents();
+
+                    // element only contains plain text
+                    if(children.length == 1 && children.get(0).nodeType == 3) {
+                        var text = c.text(),
+                            low = 0,
+                            high = text.length - 1,
+                            ellipsizedText = null;
+                        while(low <= high) {
+                            var mid = Math.floor((low + high) / 2);
+                            var str = text.substring(0, mid+1) + ellipsisText;
+                            c.text(str);
+
+                            if(fit()) {
+                                low = mid + 1;
+                                ellipsizedText = str;
+                            } else {
+                                high = mid - 1;
+                            }
+                        }
+                        
+                        if(ellipsizedText) {
+                            c.text(ellipsizedText);
+                        } else {
+                            // if no ellipsizedText found, replace current
+                            // element with ellipsis
+                            c.replaceWith(ellipsisText);
+                        }
+                        break;
+                    } else {
+                        var low = 0,
+                            high = children.length - 1,
+                            cutEl;
+                        while(low <= high) {
+                            var mid = Math.floor((low + high) / 2);
+                            c.html(addEllipsis(children, mid+1));
+
+                            if(fit()) {
+                                low = mid + 1;
+                            } else {
+                                // remove the appended ellipsis
+                                $(c.contents().last()).remove();
+                                // last element is the cut element
+                                cutEl = c.contents().get(mid);
+                                if(high == mid) {
+                                    // prevent infinite loop
+                                    break;
+                                }
+                                high = mid;
+                            }
+                        }
+
+                        if(cutEl) {
+                            c = $(cutEl);
+                        } else {
+                            // no cut Element found, hopefully this could
+                            // never happen
+                            break;
+                        }
+                    }
+                }
+
+                // set content and clean up
+                el.html(t.html());
+                t.remove();
+            }
+        }
+
+        return this.each(fnEllipsize);
+    };
+})(jQuery);
